411dd594ff06a39e5abe88faace75fd798aef68b
abhiosem <shakyaabhisek20@gmail.com>
2026-07-30 16:02:07 +0545

    Add employee routes and update function
    added new functions and routes like updates and more codes

--- Flask_project/app.py
+++ Flask_project/app.py
@@ -37,9 +37,97 @@ def submit():
             connect.close()
 
             return "<h1> Added succesfully! </h1>"
-    
-    
-    
-    
+
+@app.route("/employes")
+def open():
+        connect = sqlite3.connect("employe.db")
+        cursor = connect.cursor()
+
+        cursor.execute("SELECT * FROM employes")
+
+        employee = cursor.fetchall()
+                
+
+        connect.commit()
+        connect.close()
+
+        return render_template("employe.html",employee=employee)
+                
+@app.route("/add")
+def add():
+        return render_template("form.html")
+
+
+
+@app.route("/search")
+def search_page():
+        return render_template("search.html")
+
+
+        
+@app.route("/search", methods=["POST"])
+def search():
+        check = request.form["name"]
+
+        connect = sqlite3.connect("employe.db")
+        cursor = connect.cursor()
+
+        cursor.execute(
+            "SELECT * FROM employes WHERE name = ?",
+            (check,)
+        )
+
+        ho = cursor.fetchall()
+        print(ho)
+        connect.close()
+
+        return render_template("search.html", ho=ho)
+
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+
+    connect = sqlite3.connect("employe.db")
+    cursor = connect.cursor()
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        department = request.form["department"]
+        salary = request.form["salary"]
+
+        cursor.execute("""
+            UPDATE employes
+            SET
+                name = ?,
+                department = ?,
+                salary = ?
+            WHERE id = ?
+        """, (name, department, salary, id))
+
+        connect.commit()
+
+    cursor.execute(
+        "SELECT * FROM employes WHERE id = ?",
+        (id,)
+    )
+
+    employee = cursor.fetchone()
+
+    connect.close()
+
+    return render_template("update.html", employee=employee)
+
+
+
+
+
+
+        
+           
+     
+                       
   
 app.run(debug=True)
+       
+    
